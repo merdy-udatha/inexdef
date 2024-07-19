745843c28024bf0c513a989c1636d881a072ed91
Ian Korf <ifkorf@ucdavis.edu>
2024-07-19 13:30:46 -0700

    up

--- procgenome.py
+++ procgenome.py
@@ -13,15 +13,17 @@ d = {}
 for line in fp:
 	gid, tid, chrom, beg, end = line.split()
 	if gid not in d: d[gid] = {}
-	if tid not in d[gid]: d[gid][tid] = []
-	d[gid][tid].append((int(beg), int(end)))
+	if tid not in d[gid]: d[gid][tid] = {}
+	d[gid][tid][(int(beg), int(end))] = True
 
 # reorganize data
 # keep only the first transcript in the group
 # report the coordinates of exon1-intron-exon2
 for gid in d:
 	tid = list(d[gid].keys())[0]
-	exons = d[gid][tid]
+	exons = list(d[gid][tid].keys())
+	exons.sort(key=lambda tup: tup[0])
+	
 	if len(exons) == 1: continue # no intron
 	if exons[0][0] > exons[1][0]: exons = list(reversed(exons)) # neg strand
 	
@@ -30,7 +32,5 @@ for gid in d:
 		e2b, e2e = exons[i+1]
 		ib = e1e+1
 		ie = e2b-1
-		print(gid, tid, e1b, e1e, ib, ie, e2b, e2e)
-		if (ib > ie): sys.exit('ERROR: negative strand length')
-
-	
+		if (ib > ie): sys.exit('ERROR: negative strand length')			
+		print(f'{gid}\t{tid}\t{e1b}\t{e1e}\t{ib}\t{ie}\t{e2b}\t{e2e}')
